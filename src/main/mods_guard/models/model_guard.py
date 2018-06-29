dc12bc834138d9cb2d525a75ad62d5de46214428
Yunzhi Tan <tyz.tantan@gmail.com>
2018-06-29 23:00:02 +0800

    remove unuseful code

--- src/main/mods_guard/models/model_guard.py
+++ src/main/mods_guard/models/model_guard.py
@@ -1,49 +1,5 @@
 from urllib.parse import quote
-import json
 import requests
-
-receiver_real_time_feature_indices = {
-    "receiver_popularity": "41001",
-    "distance": "41005",
-    "receiver_minutes_swiped_minus_last_active": "41004"
-}
-
-actor_real_time_feature_indices = {
-    "actor_popularity": "41001"
-}
-
-
-def combine_real_time_features(features, indices, real_time_features):
-    real_features = ",".join([indices[ele] + "#" + str(real_time_features[ele]) for ele in
-                              filter(lambda x: x in real_time_features, indices)])
-    if real_features != "":
-        if "##fFloat##" in features:
-            pos = features.index("##fFloat##")
-            features = features[:pos] + "##fFloat##" + real_features + "," + features[pos + 10:]
-        else:
-            if len(features) > 0:
-                features = features + "##fFloat##" + real_features
-            else:
-                features = "fFloat##" + real_features
-
-    return features
-
-
-def merge_real_time_features(actor_features, receiver_features, real_time_features):
-    if real_time_features != "" and real_time_features[0] == "{":
-        real_time_features = json.loads(real_time_features)
-    else:
-        return actor_features, receiver_features, real_time_features
-
-    if "receiver_minutes_swiped_minus_last_active" in real_time_features:
-        real_time_features["receiver_minutes_swiped_minus_last_active"] *= 60
-
-    actor_features = combine_real_time_features(actor_features, actor_real_time_feature_indices, real_time_features)
-    receiver_features = combine_real_time_features(receiver_features, receiver_real_time_feature_indices,
-                                                   real_time_features)
-    real_time_features = ""
-
-    return actor_features, receiver_features, real_time_features
 
 
 def get_score(service, test_data):
@@ -54,9 +10,6 @@ def get_score(service, test_data):
     receiver_features = test_data["receiver_features"]
     real_time_features = test_data["real_time_features"]
 
-    actor_features, receiver_features, real_time_features = merge_real_time_features(actor_features,
-                                                                                     receiver_features,
-                                                                                     real_time_features)
     test_data["actor_features"] = actor_features
     test_data["receiver_features"] = receiver_features
     test_data["real_time_features"] = real_time_features
