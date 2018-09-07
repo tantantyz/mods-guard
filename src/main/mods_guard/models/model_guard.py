f94c92c83a2981b73833d45c1e61654b843f5fbb
Yunzhi Tan <tyz.tantan@gmail.com>
2018-09-07 14:12:23 +0800

    add more parameters when debugging

--- src/main/mods_guard/models/model_guard.py
+++ src/main/mods_guard/models/model_guard.py
@@ -13,9 +13,11 @@ def get_score(service, test_data):
     test_data["actor_features"] = actor_features
     test_data["receiver_features"] = receiver_features
     test_data["real_time_features"] = real_time_features
+    print("actor_features", actor_features)
+    print("receiver_features", receiver_features)
+    print("realtime_features", real_time_features)
 
-    url = "http://%s:8008/test/scorePlayer?modelId=%s&actorFeature=%s&receiverFeature=%s&realTimeFeature=%s" % (
-        service, model_id, quote(actor_features), quote(receiver_features), quote(real_time_features)
+    url = "http://%s:8008/test/scorePlayer?actorId=%d&receiverId=%d&modelId=%s&isDebugging=true&actorFeature=%s&receiverFeature=%s&realTimeFeature=%s" % (
+        service, actor_user_id, receiver_user_id, model_id, quote(actor_features), quote(receiver_features), quote(real_time_features)
     )
-    # print(url)
     return requests.get(url)
